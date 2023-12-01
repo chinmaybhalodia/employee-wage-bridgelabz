c5315a1444463ad63f472ffd84f14639743268bb
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-01 19:42:30 +0530

    added daily wage methods and params

--- src/App.java
+++ src/App.java
@@ -3,9 +3,12 @@ public class App {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         // employee e1
-        Employee e1 = new Employee();
+        Employee e1 = new Employee("FULL");
 
         // checking attendance of employee e1
         System.out.println(e1.checkAttendance());
+
+        // getting daily full time wage
+        e1.getFullTimeWage();
     }
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,10 @@
 public class Employee {
-    public Employee() {
+    static int wage_per_hour = 20;
+    static int full_time_hours = 8;
+    String type; // FULL for full time employee and PART for part time employee
 
+    public Employee(String type) {
+        this.type = type;
     }
 
     // UC1 : function to check employee attendance
@@ -9,5 +13,15 @@ public class Employee {
         if (val > 0.5)
             return true;
         return false;
+    }
+
+    // UC2: function to get full time employee wage
+    public void getFullTimeWage() {
+        if (!this.type.equals("FULL")) {
+            System.out.println("The employee is not full time employee.");
+        } else {
+            int dailywage = full_time_hours * wage_per_hour;
+            System.out.println("Daily wage for this full time employee is: " + dailywage);
+        }
     }
 }
